4d91d7ae72b5ffd20a8fd768c183364d4cb23560
CTC3PO <chautrancmt26@gmail.com>
2022-10-21 12:36:11 -0400

    edit the About page

--- src/pages/About.js
+++ src/pages/About.js
@@ -5,11 +5,9 @@ export default function About() {
     <section className="section about-section">
       <h1 className="section-title">about us</h1>
       <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae
-        repudiandae architecto qui adipisci in officiis, aperiam sequi atque
-        perferendis eos, autem maiores nisi saepe quisquam hic odio consectetur
-        nobis veritatis quasi explicabo obcaecati doloremque? Placeat ratione
-        hic aspernatur error blanditiis?
+        The cocktail database is a simple web app that get cocktails from the
+        TheCocktailDB.com APIs. The app displays the recipe details and also let
+        user search for their favorite recipe. The web app is hosted on netlify.
       </p>
     </section>
   );
